5a57ced6f93078e6ddfc7fe0742588de55b108eb
Alp Eren Sarı <sari.eren@metu.edu.tr>
2019-01-04 20:46:41 +0300

    new features of the gui imlemented. Programs now asks user to continue, to change the maximum iteration and inital function

--- my_gui.m
+++ my_gui.m
@@ -22,7 +22,7 @@ function varargout = my_gui(varargin)
 
 % Edit the above text to modify the response to help my_gui
 
-% Last Modified by GUIDE v2.5 01-Jan-2019 23:54:45
+% Last Modified by GUIDE v2.5 04-Jan-2019 19:55:54
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -124,19 +124,56 @@ function run_Callback(hObject, eventdata, handles)
 % hObject    handle to run (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-N = round(str2double(get(handles.N, 'string')));
-M = str2double(get(handles.M, 'string'));
+opt.N = round(str2double(get(handles.N, 'string')));
+opt.M = str2double(get(handles.M, 'string'));
 i_max = str2double(get(handles.it_max, 'string'));
-method = get(get(handles.uibuttongroup2, 'SelectedObject'), 'Tag');
-solver = get(get(handles.uibuttongroup1, 'SelectedObject'), 'Tag');
-search = get(get(handles.uibuttongroup4, 'SelectedObject'), 'Tag');
+opt.initial = get(handles.initial_y, 'string');
+opt.method = get(get(handles.uibuttongroup2, 'SelectedObject'), 'Tag');
+opt.solver = get(get(handles.uibuttongroup1, 'SelectedObject'), 'Tag');
+opt.search = get(get(handles.uibuttongroup4, 'SelectedObject'), 'Tag');
+opt.loop = 0;
 % disp(N);disp(M);disp(it_max);
-[x_sol, iteration] = take_home_func(N, M, method, solver, search, i_max);
-x = linspace(0,5,N);
-plot(x, x_sol);
-title('The Minimum Surface Function');
-xlabel('x');ylabel('y');
+while(1)
+    tic
+    [x_sol, iteration, y0] = take_home_func(opt, i_max);
+    elapsed_time = toc;
+    elapsed_text = [num2str(elapsed_time), ' s'];
+    set(handles.text_time, 'string', elapsed_text); 
+    x = linspace(0,5,opt.N);
 
+    axes(handles.axes1); % switch to axes
+    plot(x, y0);
+    title('The Initial Function');
+    xlabel('x');ylabel('y');
+
+    axes(handles.axes2); % switch to axes
+    plot(x, x_sol);
+    title('The Minimum Surface Function');
+    xlabel('x');ylabel('y');
+
+    answer = questdlg('Would you like to continue the optimization process?', ...
+        'Make Your Choice', ...
+        'Yes','No', 'No');
+
+    opt.loop = 1;
+    if(strcmp(answer, 'No'))
+        break;
+    else
+        answer_it = inputdlg({'Please enter the new maximum iteration'});
+        i_max = round(str2num(answer_it{1}));
+        answer_in = questdlg('Would you like to use select a new inital function?', ...
+        'Make Your Choice', ...
+        'Yes','No, use the last point', 'No, use the last point');
+        if(strcmp(answer, 'Yes'))
+            answer_initial = inputdlg({'Please enter the new inital function'});
+            opt.loop = 0;
+            opt.initial = sprintf(answer_initial{1});
+        else
+            opt.initial = x_sol;
+        end
+
+    end
+end
 
 % --- Executes when selected object is changed in uibuttongroup1.
 function uibuttongroup1_SelectionChangedFcn(hObject, eventdata, handles)
@@ -160,6 +197,36 @@ function it_max_Callback(hObject, eventdata, handles)
 % --- Executes during object creation, after setting all properties.
 function it_max_CreateFcn(hObject, eventdata, handles)
 % hObject    handle to it_max (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes during object creation, after setting all properties.
+function text_time_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to text_time (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+
+
+function initial_y_Callback(hObject, eventdata, handles)
+% hObject    handle to initial_y (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of initial_y as text
+%        str2double(get(hObject,'String')) returns contents of initial_y as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function initial_y_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to initial_y (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    empty - handles not created until after all CreateFcns called
 
